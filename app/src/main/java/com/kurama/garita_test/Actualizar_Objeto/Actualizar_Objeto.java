ebf9ee79b160875a6f185a5438b058263a469a50
kurama <robertoflores@upeu.edu.pe>
2022-11-18 21:48:43 -0500

    ya se pueden ver los estados en listar

---
+++ app/src/main/java/com/kurama/garita_test/Actualizar_Objeto/Actualizar_Objeto.java
@@ -0,0 +1,16 @@
+package com.kurama.garita_test.Actualizar_Objeto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kurama.garita_test.R;
+
+public class Actualizar_Objeto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actualizar_objeto);
+    }
+}
